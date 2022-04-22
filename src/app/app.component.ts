a8d25ac68594a3a15cc5d8d8740017a16d08bc94
nevesw <wneves@outlook.com>
2022-04-22 14:38:08 -0300

    primeiro commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+  transferencias: any[] = [];
+
+  transferir($event){
+    //console.log($event);
+    const transferencia = {...$event, data: new Date()};
+    console.log(transferencia);
+    this.transferencias.push(transferencia);
+  }
 }
